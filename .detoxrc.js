5e181e3360a02c05ffb813a561faf99643d0645b
Josh Justice <me@codingitwrong.com>
2023-03-24 15:42:49 -0400

    Match up emulators

--- .detoxrc.js
+++ .detoxrc.js
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_30'
+        avdName: 'Pixel_API_29_AOSP'
       }
     }
   },

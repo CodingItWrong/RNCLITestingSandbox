23fe2da7d37c5f903c941721313e40885721f832
Josh Justice <me@codingitwrong.com>
2023-10-10 05:57:44 -0400

    Update generated test to pass

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -8,16 +8,8 @@ describe('Example', () => {
   });
 
   it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+    await expect(
+      element(by.text('Read the docs to discover what to do next:')),
+    ).toBeVisible();
   });
 });
